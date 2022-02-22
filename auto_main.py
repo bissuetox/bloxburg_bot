56621786b93ef58af7dacb02876e9a4127570294
bissuetox <bissuetox@gmail.com>
2022-02-21 18:07:25 -0800

    prog

--- auto_main.py
+++ auto_main.py
@@ -1,6 +1,19 @@
 import pyautogui as pg
 import time
+import keyboard
+# import win32api, win32con
+import cv2 as cv
 
-for i in range(10):
-    print(pg.position())
+# for i in range(50):
+#     if pg.locateOnScreen('img/burger1.png', 0.5) != None:
+#         print("In view!")
+#     else:
+#         print("Found nothing")
+#     time.sleep(0.1)
+
+while True:
     time.sleep(0.1)
+    print(pg.position())
+
+    # x 300 - 700
+    # y 430 - 900
