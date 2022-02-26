0da042e82aa8d046f3b10d1a452de9b3769c669d
bissuetox <bissuetox@gmail.com>
2022-02-25 19:26:18 -0800

    functioning, need to implement action class memeber, and threading

--- bloxburg_cashier.py
+++ bloxburg_cashier.py
@@ -2,13 +2,17 @@ import pyautogui as pg
 import pydirectinput as pdi
 import time
 import keyboard
+import os
+import json
 from datetime import datetime
 
 class bbimg:
-    def __init__(self, pos_tuple=(), prompt_img_path="", prompt_region=()):
+    def __init__(self, key=None, pos_tuple=(), prompt_img_path="", prompt_region=(), debug=False):
+        self.key=key
         self.pos_tuple = pos_tuple
         self.prompt_img_path = prompt_img_path
         self.prompt_region = prompt_region
+        self.debug = False
 
     def locate(self):
         val = pg.locateOnScreen(self.prompt_img_path, confidence=0.9, grayscale=True, region=self.prompt_region)
@@ -16,97 +20,104 @@ class bbimg:
 
     def click(self):
         pdi.moveTo(self.pos_tuple[0], self.pos_tuple[1])
-        time.sleep(0.05) # Move twice for good measure - buggy
         pdi.moveTo(self.pos_tuple[0]+1, self.pos_tuple[1]+1)
+        time.sleep(0.01) # Move twice for good measure - buggy
         pdi.click()
 
-def screenshot(filepath="test.png", region=None):
-    pic = pg.screenshot(region=region)
-    pic.save(filepath)
+    def check(self):
+        if self.locate():
+            print(f"Found {self.key}!")
+            self.click()
+            return True
+        else:
+            return False
 
-def loop_locate(objs, det_timeout=50, debug=False):
+def screenshot(filename="screenshot", include_date=True, region=None):
+    pic = pg.screenshot(region=region)
+    now = datetime.now().strftime("%m-%d-%Y_%H-%M-%S") if include_date else ""
+    path = os.path.join(os.getcwd(), "screenshots", f"{filename}_{now}.png")
+    pic.save(path)
+
+def loop_locate(objs, det_timeout=50):
     order_open = False
     no_detections = 0
 
     while keyboard.is_pressed('q') == False:
-        if objs["b1"].locate():
-            if debug: print("found burger 1!")
-            objs["b1"].click()
-            order_open = True
-
-        if objs["b2"].locate():
-            if debug: print("found burger 2!")
-            objs["b2"].click()
-            order_open = True
-
-        if objs["b3"].locate():
-            if debug: print("found burger 3!")
-            objs["b3"].click()
-            order_open = True
-
-        if objs["fries"].locate():
-            if debug: print("found fries")
-            objs["fries"].click()
-            order_open = True
-
-        if objs["soda"].locate():
-            if debug: print("found soda")
-            objs["soda"].click()
-            order_open = True
+        for key, obj in objs.items():
+            if obj.key != "done":
+                order_open = True if obj.check() else order_open
 
         if order_open:
             objs["done"].click()
-            if debug: print("clicked done!")
+            print("clicked done!")
             order_open = False
             no_detections = 0
+            time.sleep(1)
 
         else:
-            if debug: print("none found")
+            print("none found")
             no_detections += 1
             time.sleep(0.5)
 
         # Take a screenshot if something gets in the way
-        if no_detections >= det_timeout:
-            now = datetime.now().strftime("%H-%M-%S")
-            screenshot(filepath=f"no_detection_{now}.png")
-            exit("Detection timed out!")
+        if no_detections == det_timeout:
+            screenshot(filename="no_detection")
+            # exit("Detection timed out!")
+
 
     print("Program Stopped")
 
-def setup_objects(prompt_region):
+def setup_objects(config, prompt_region, debug=False):
     objs = {}
-
+    objs_dict = config["objects"]
     print("Hover over items then hit ALT\n")
 
-    print("Burger 1 - ", end="", flush=True)
-    keyboard.wait("alt")
-    objs["b1"] = bbimg(pg.position(), 'img/b1.png', prompt_region)
-    print(pg.position())
+    for key, obj in objs_dict.items():
+        # Handle multiple types of detection?
+        if obj["type"] == "button":
+            # Fetch position of button
+            print(obj["label"] + " - ", end="", flush=True)
+            keyboard.wait(config["set_key"])
+            pos = pg.position()
 
-    print("Burger 2 - ", end="", flush=True)
-    keyboard.wait("alt")
-    objs["b2"] = bbimg(pg.position(), 'img/b2.png', prompt_region)
-    print(pg.position())
+            objs[key] = bbimg(
+                            key=key,
+                            pos_tuple=pos,
+                            prompt_img_path=obj["detect_img_path"],
+                            prompt_region=prompt_region,
+                            debug=debug
+                        )
+            print(pos)
+        
 
-    print("Burger 3 - ", end="", flush=True)
-    keyboard.wait("alt")
-    objs["b3"] = bbimg(pg.position(), 'img/b3.png', prompt_region)
-    print(pg.position())
+    # keyboard.wait("alt")
+    # objs["b1"] = bbimg(pg.position(), 'img/b1.png', prompt_region)
+    # print(pg.position())
 
-    print("Fries - ", end="", flush=True)
-    keyboard.wait("alt")
-    objs["fries"] = bbimg(pg.position(), 'img/f.png', prompt_region)
-    print(pg.position())
+    # print("Burger 2 - ", end="", flush=True)
+    # keyboard.wait("alt")
+    # objs["b2"] = bbimg(pg.position(), 'img/b2.png', prompt_region)
+    # print(pg.position())
 
-    print("Soda - ", end="", flush=True)
-    keyboard.wait("alt")
-    objs["soda"] = bbimg(pg.position(), 'img/s.png', prompt_region)
-    print(pg.position())
+    # print("Burger 3 - ", end="", flush=True)
+    # keyboard.wait("alt")
+    # objs["b3"] = bbimg(pg.position(), 'img/b3.png', prompt_region)
+    # print(pg.position())
 
-    print("Done button - ", end="", flush=True)
-    keyboard.wait("alt")
-    objs["done"] = bbimg(pg.position(), '')
-    print(pg.position())
+    # print("Fries - ", end="", flush=True)
+    # keyboard.wait("alt")
+    # objs["fries"] = bbimg(pg.position(), 'img/f.png', prompt_region)
+    # print(pg.position())
+
+    # print("Soda - ", end="", flush=True)
+    # keyboard.wait("alt")
+    # objs["soda"] = bbimg(pg.position(), 'img/s.png', prompt_region)
+    # print(pg.position())
+
+    # print("Done button - ", end="", flush=True)
+    # keyboard.wait("alt")
+    # objs["done"] = bbimg(pg.position(), '')
+    # print(pg.position())
 
     return objs
 
@@ -125,3 +136,10 @@ def get_prompt_window_region():
     print(f"W: {width}, H: {height}")
     # (left, top, width, height)
     return (tl[0], tl[1], width, height)
+
+def parse_json(path="config.json"):
+    f = open(path)
+    data = json.load(f)
+    f.close()
+    return data
+    
--- cashier.py
+++ cashier.py
@@ -1,9 +1,13 @@
 from bloxburg_cashier import *
 
 if __name__ == "__main__":
-    prompt_region = get_prompt_window_region()
-    bbimg.prompt_region = prompt_region
-    # screenshot(prompt_region)
-
-    objs = setup_objects(prompt_region)
-    loop_locate(objs=objs, det_timeout=200, debug=False)
+    config = parse_json("config.json")
+    debug_bool = bool(config["debug"])
+    timeout = int(config["detection_timeout"])
+    
+    try:
+        prompt_region = get_prompt_window_region()
+    except ValueError as e:
+        print("Bad Region Dimensions!")
+    objs = setup_objects(config, prompt_region, debug=debug_bool)
+    loop_locate(objs=objs, det_timeout=timeout)
